12da18c9963941f5cc0cde8b98e9d22d3ef19532
Tom R <tomas.ruzicka@turnus.cz>
2017-02-09 13:57:51 +0100

    Zkušební last commit

--- finalboot/src/main/java/finalppro/dao/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package finalppro.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import mybootppro.model.Task;	
-
-public interface TaskRepository extends CrudRepository<Task, Integer>{
-
-}
